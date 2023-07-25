02e0de783def122780e9ea6d1c35c99ab7c531a1
IlgazCvdr <icavdare@gmail.com>
2023-07-25 17:47:40 +0300

    Update are done only main methods remain for updates

--- src/main/java/com/ilgazProject/movieApp/MovieAppApplication.java
+++ src/main/java/com/ilgazProject/movieApp/MovieAppApplication.java
@@ -7,33 +7,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class MovieAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovieAppApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieAppApplication.class, args);
-	}
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args, MovieDao movieDao) {
+        return runner -> {
+            //createMovie(movieDao, "dirName", "movName", 0);
+            //findMovieById(movieDao, 0);//id ve movie data acc
+            //findMovieByDirector(movieDao,"directorName")
+        };
+    }
 
-	@Bean
-	public CommandLineRunner commandLineRunner(String[] args, MovieDao movieDao){
-		return runner -> {
-			//createMovie(movieDao, "dirName", "movName", 0);
-			findMovieById(movieDao,0);//id ve movie data access object
-		};
-	}
+    public void createMovie(MovieDao movieDao, String dirName, String movieName, double rating) {
+        //create a movie object
+        Movie tempMovie = new Movie(dirName, movieName, rating);
+        //save the movie object
+        movieDao.save(tempMovie);
+        System.out.println(tempMovie + " is saved");
+    }
 
-	public void createMovie(MovieDao movieDao, String dirName, String movieName, double rating){
-		//create a movie object
-		Movie tempMovie = new Movie(dirName,movieName,rating);
-		//save the movie object
-		movieDao.save(tempMovie);
-		//
-		System.out.println(tempMovie.toString() + " is saved");
-	}
-	public Movie findMovieById(MovieDao movieDao,Integer  id){
-		Movie tempMovie = movieDao.findById(id);
+    public Movie findMovieById(MovieDao movieDao, Integer id) {
+        Movie tempMovie = movieDao.findById(id);
+        System.out.println("The movie id found. Its values are " + tempMovie.toString());
+        return tempMovie;
+    }
 
-		System.out.println("The movie id found. Its values are " + tempMovie.toString());
-		return tempMovie;
-	}
+    public List<Movie> findMovieByDirector(MovieDao movieDao, String directorName) {
+        List<Movie> tempMovieList = movieDao.findByDirector(directorName);
+        for (Movie movie : tempMovieList) {
+            System.out.println(movie.getMovieName());
+        }
+        return tempMovieList;
+    }
 }
